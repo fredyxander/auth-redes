4065a109affb50395f4aab24ce1d428a55d691c3
fredyxander <freddy5210@gmail.com>
2023-02-27 19:03:35 -0500

    firts commit

---
+++ src/config/options.js
@@ -0,0 +1,7 @@
+const options = {
+    mongoDB:{
+        url:"mongodb+srv://fredy:coder@coderbackend.d0kaklh.mongodb.net/authDB?retryWrites=true&w=majority"
+    }
+};
+
+module.exports = {options};
